20b51d58957f9cee67c440eac4bc6d01ae65c0a9
Oganes <og.marckaryan@yandex.ru>
2025-01-29 21:05:06 +0300

    написал функцию демонстрации слова при правильном ответе

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 import getRandomWord from "./js/getRandomWord";
-import createCharPanel from "./js/createCharPanel"
+import createCharPanel from "./js/createCharPanel";
 import createFieldWord from "./js/createFieldWord";
 import displayWord from "./js/displayWord";
-import checkCharInWord from "./js/checkCharInWord"
-import "./style/style.css"
+import checkCharInWord from "./js/checkCharInWord";
+import showLetter from "./js/showLetter";
+import "./style/style.css";
 
-createCharPanel()
+createCharPanel();
 
-let randomWord = getRandomWord()
-displayWord(randomWord)
-console.log(randomWord) 
-createFieldWord()
-let allChar = document.querySelectorAll('.target-char')
+let randomWord = getRandomWord();
+displayWord(randomWord);
+console.log(randomWord);
+createFieldWord();
+let allChar = document.querySelectorAll(".target-char");
 
-allChar.forEach((e)=>{
-    e.addEventListener('click', (currentEl)=>{
-        currentEl.target.style.color = 'red'
-        console.log(currentEl.target.textContent)
-        console.log(checkCharInWord(randomWord,currentEl.target))
-    })
-})
+allChar.forEach((e) => {
+  e.addEventListener("click", (currentEl) => {
+    let el = currentEl.target;
+    if (checkCharInWord(randomWord, el)) {
+      el.style.color = "green";
+      showLetter(randomWord, el);
+    } else {
+      el.style.color = "red";
+    }
+  });
+});
---
+++ js/showLetter.js
@@ -0,0 +1,7 @@
+export default function showLetter(word,letter){
+    for (let i = 0; i < word.length; i+=1){
+        if(word[i] == letter.textContent){
+            document.querySelector('.word').children[i].textContent = letter.textContent
+        }
+    }
+}
